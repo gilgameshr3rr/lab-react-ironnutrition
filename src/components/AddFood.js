89a2f665f726aeb9d4b9dfc7f7b51d00c98c0455
Larissa Ferreira <larissadantasaf@gmail.com>
2022-02-10 13:28:36 +0100

    Iteration 1 and 2

---
+++ src/components/AddFood.js
@@ -0,0 +1,12 @@
+// import { useState } from 'react';
+
+// function AddFood(){
+//     const [form, setForm] = useState({
+//     name: "",
+//     calories: "",
+//     image: ""
+//     })
+
+// }
+
+// export default AddFood;
